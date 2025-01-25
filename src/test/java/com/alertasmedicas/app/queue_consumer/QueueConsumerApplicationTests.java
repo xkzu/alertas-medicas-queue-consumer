680e5ef6ff9316a2136807ffe0f02d00ebd443b4
Kzu <alvaro.vega.pavez@gmail.com>
2025-01-25 03:51:16 -0300

    add: main test

---
+++ src/test/java/com/alertasmedicas/app/queue_consumer/QueueConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertasmedicas.app.queue_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
