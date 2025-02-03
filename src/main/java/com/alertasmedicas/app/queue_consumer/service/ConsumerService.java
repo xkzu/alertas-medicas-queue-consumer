63b3248fb3e4e9dd2a1a10e64976fef1fede8f80
Kzu <alvaro.vega.pavez@gmail.com>
2025-02-03 15:41:09 -0300

    add: ConsumerService con logica para recibir anomalia desde la cola y guardarla en bd

---
+++ src/main/java/com/alertasmedicas/app/queue_consumer/service/ConsumerService.java
@@ -0,0 +1,58 @@
+package com.alertasmedicas.app.queue_consumer.service;
+
+import com.alertasmedicas.app.queue_consumer.dto.MeasurementDTO;
+import com.alertasmedicas.app.queue_consumer.util.MessageParser;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+@Service
+public class ConsumerService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${api.measurement:}")
+    private String domain;
+
+    @Autowired
+    public ConsumerService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RabbitListener(queues = "#{queueAnomaly}")
+    public void receiveMessageAnomaly(String message) {
+        log.info("Mensaje recibido cola anomaly: {}", message);
+        saveAnomaly(message);
+    }
+
+    private void saveAnomaly(String message) {
+        log.info("Guardando anomalia en bd: {}", message);
+        MeasurementDTO measurementDTO = MessageParser.parseMeasurement(message);
+        log.info("Parseando message a measurement: {}", measurementDTO);
+        MeasurementDTO measurementSaved = saveMeasurement(measurementDTO);
+        log.info("Anomalia guardada: {}", measurementSaved);
+    }
+
+    private MeasurementDTO saveMeasurement(MeasurementDTO measurementDTO) {
+        String url = domain + "/measurement/add";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<MeasurementDTO> requestEntity = new HttpEntity<>(measurementDTO, headers);
+        ResponseEntity<MeasurementDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                MeasurementDTO.class
+        );
+        return response.getBody();
+    }
+}
